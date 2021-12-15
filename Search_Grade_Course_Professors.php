6cdd9d488bb191c644d2c9eddd2a98ebe2ec46e5
hojichamt <78385842+hojichamt@users.noreply.github.com>
2021-12-15 12:47:25 -0800

    Fixed SQL statement

--- Search_Grade_Course_Professors.php
+++ Search_Grade_Course_Professors.php
@@ -1,4 +1,3 @@
-
 <head>
 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.13.0/themes/base/jquery-ui.css">
@@ -24,98 +23,51 @@ border-width:5px;
 
 include "Connect.php";
 
+$keywordfromform_Prof_Course= $_GET["keyword_Section_Num"];
 $keywordfromform_Prof_Section= $_GET["keyword_Prof_Grades"];
 
-$keywordfromform_Prof_Course= $_GET["keyword_Section_Num"];
+echo "Searching For:<br>";
+echo "Course Number: ".$keywordfromform_Prof_Course."<br>";
+echo "Section Number: ".$keywordfromform_Prof_Section."<br>";
 
-
-echo "<h2> Professor Information </h2>";
-
-
+//Search database for Course and Section
 
 $sql = "SELECT Grade, COUNT(*) as count
 FROM Enrollment_Records INNER JOIN Section
 ON Enrollment_Records.Section_Num = Section.Section_Num
-WHERE Section.Course_Num LIKE '%.$keywordfromform_Prof_Course.%'
-AND Section.Section_Num LIKE '%.$keywordfromform_Prof_Section.%'
+WHERE Section.Course_Num LIKE '%".$keywordfromform_Prof_Course."%'  AND Section.Section_Num LIKE '%".$keywordfromform_Prof_Section."%'
 GROUP BY Grade";
 
 $result = $mysqli->query($sql);
 
 if ($keywordfromform_Prof_Course == NULL|| $keywordfromform_Prof_Section == NULL) {
   
+	echo "<h2> Please enter a Course and Section </h2>";
 
-    echo " Please enter a Course and Section";
+	} else if ($result->num_rows > 0) {
   
+		echo "<h2> Showing Grades and Student Count </h2>";
+		// output data of each row
+		echo "<table border='1'>
+			<tr>
+			<th>Grade</th>
+			<th>Students with Grade</th>
+			</tr>";
 
-  }else if ('') {
-  // output data of each row
-
-  while($row = $result->fetch_assoc()) {
-    echo "<table border='1'>
-    
-
-    <tr>
-    <th>Grade</th>
-   
-    </tr>";
-   
-   
-    echo "<tr>";
-      echo "<td>" . $row['Grade'] . "</td>";
-      echo "</tr>";
-
-
-  }
+		while($row = $result->fetch_assoc()) {
+	   
+			echo "<tr>";
+			echo "<td>" . $row['Grade'] . "</td>";
+			echo "<td>" . $row['count'] . "</td>";
+			echo "</tr>";
+     
+		}
+  
 } 
+else {
+	
+	echo "<h2> Cannot Find Course and Section </h2>";
+  
+}
 
 ?>
-
-<!---
-echo "<table border='1'>
-<tr>
-<th>A+</th>
-<th>A</th>
-<th>A-</th>
-<th>B+</th>
-<th>B</th>
-<th>B-</th>
-<th>C+</th>
-<th>C</th>
-<th>C-</th>
-<th>D+</th>
-<th>D</th>
-<th>D-</th>
-<th>F+</th>
-<th>F</th>
-<th>F-</th>
-</tr>";
-echo "<tr>";
-
-
-echo "</tr>";
-
-
-
-
-
-
-
-<table class="table table-striped table-bordered classes">
-<thead>
-<tr>
-<th>A</th>
-<th>A-</th>
-<th>B+</th>
-<th>B</th>
-<th>B-</th>
-<th>C+</th>
-<th>C</th>
-<th>C-</th>
-<th>D+</th>
-<th>D</th>
-<th>D-</th>
-<th>F</th>
-</tr>
- </thead>
--->
